eeea4728db0025b8315fef1e0e29da4c28c958e4
Amalahasan M <amalkassan@gmail.com>
2024-11-01 08:55:40 +0530

    Extract rar

---
+++ MauiAppiOSSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppiOSSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppiOSSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppiOSSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
